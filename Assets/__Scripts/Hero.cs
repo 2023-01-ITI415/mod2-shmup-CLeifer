815381a81c283844db585bc037691803d1b0527e
CLeifer <cml370@scarletmail.rutgers.edu>
2023-02-22 16:36:17 -0500

    Adjusted how shields work

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -13,7 +13,11 @@ public class Hero : MonoBehaviour
 
     [Header("Dynamic")]
     [Range(0, 4)]
-    public float shieldLevel = 1;
+    [SerializeField]
+    private float _shieldLevel = 1;
+
+    [Tooltip("This field holds a reference to the last triggering GameObject")]
+    private GameObject lastTriggerGo = null;
 
     void Awake()
     {
@@ -38,5 +42,41 @@ public class Hero : MonoBehaviour
         transform.position = pos;
 
         transform.rotation = Quaternion.Euler(vAxis * pitchMult, hAxis * rollMult, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+
+        if (go == lastTriggerGo) return;
+        lastTriggerGo = go;
+
+        Enemy enemy = go.GetComponent<Enemy>();
+        if(enemy != null)
+        {
+            shieldLevel--;
+            Destroy(go);
+        }
+        else
+        {
+            Debug.LogWarning("Shield trigger hit by non-Enemy:" + go.name);
+        }
+    }
+
+    public float shieldLevel
+    {
+        get
+        {
+            return (_shieldLevel);
+        }
+        private set
+        {
+            _shieldLevel = Mathf.Min(value, 4);
+            if(value < 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
     }
 }
