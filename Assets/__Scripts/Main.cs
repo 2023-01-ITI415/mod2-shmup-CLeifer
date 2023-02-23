9fc9cc57e2b450159795b00d9d5814e057a854a8
CLeifer <cml370@scarletmail.rutgers.edu>
2023-02-23 00:00:29 -0500

    Created Projectile

--- Assets/__Scripts/Main.cs
+++ Assets/__Scripts/Main.cs
@@ -11,6 +11,7 @@ public class Main : MonoBehaviour
     public GameObject[] prefabEnemies;
     public float enemySpawnPerSecond = 0.5f;
     public float enemyInsetDefault = 1.5f;
+    public float gameRestartDelay = 2;
 
     private BoundsCheck bndCheck;
 
@@ -42,5 +43,20 @@ public class Main : MonoBehaviour
 
         Invoke(nameof(SpawnEnemy), 1f / enemySpawnPerSecond);
 
+    }
+
+    void DelayedRestart()
+    {
+        Invoke(nameof(Restart), gameRestartDelay);
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene("_Scene_0");
+    }
+
+    static public void HERO_DIED()
+    {
+        S.DelayedRestart();
     }
 }
